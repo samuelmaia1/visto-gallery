59280e093a7e28f3a35986605ceaad39c73ef032
Samuel Maia <153786965+samuelmaia1@users.noreply.github.com>
2025-09-09 22:06:31 -0300

    style: add variable and global style

---
+++ src/styles/Global.ts
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+export const variables = {
+    colors: {
+        black: '#333',
+        white: '#FFF',
+        orange: '#D7540D'
+    },
+    fonts: {
+        inter: 'Inter-VariableFont_opsz,wght',
+        montserrat: 'Montserrat-Regular'
+    }
+}
+
+export const global = StyleSheet.create({
+    container: {
+        paddingHorizontal: 30,
+        paddingVertical: 50,
+        backgroundColor: '#FFF'
+    },
+    text: {
+        color: variables.colors.black,
+        fontSize: 16,
+        fontFamily: variables.fonts.montserrat
+    }
+})
+
