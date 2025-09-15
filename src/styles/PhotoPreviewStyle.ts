25de67b70ac64ea1a6b84cd580a7bf34970820e3
Samuel Maia <153786965+samuelmaia1@users.noreply.github.com>
2025-09-15 11:23:15 -0300

    ajust photo preview style

--- src/styles/PhotoPreviewStyle.ts
+++ src/styles/PhotoPreviewStyle.ts
@@ -5,7 +5,7 @@ import { variables } from "./GlobalStyle";
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
-        paddingTop: 70,
+        paddingTop: 50,
         paddingHorizontal: 30
     },
     image: {
